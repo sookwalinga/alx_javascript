0576211f415a267882636c9c172ea9d58b4449d8
sam <sookwalinga@gmail.com>
2023-09-06 15:08:50 +0300

    test

---
+++ javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function incrementAndCall(number, theFunction) {
+  const incrementedNumber = number + 1
+  theFunction(incrementedNumber)
+}
+
+module.exports = { incrementAndCall }
