fd9429c6bf99586f3611e425b8134211db5b259f
sam <sookwalinga@gmail.com>
2023-09-13 13:42:04 +0300

    refactor: task2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,11 +1,12 @@
 const request = require('request')
 
 if (process.argv.length !== 3) {
-  console.error('Usage: node count-movies.js <API URL>')
+  console.error('Usage: node 2-starwars_count.js <API URL>')
   process.exit(1)
 }
 
 const apiUrl = process.argv[2]
+const characterId = 18
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -19,16 +20,14 @@ request.get(apiUrl, (error, response, body) => {
   }
 
   try {
-    const data = JSON.parse(body)
-    const moviesWithWedgeAntilles = data.results.filter((movie) =>
+    const movies = JSON.parse(body).results
+    const moviesWithWedgeAntilles = movies.filter((movie) =>
       movie.characters.includes(
-        'https://swapi-api.alx-tools.com/api/people/18/'
+        `https://swapi-api.alx-tools.com/api/people/${characterId}/`
       )
     )
 
-    console.log(
-      `Number of movies with Wedge Antilles: ${moviesWithWedgeAntilles.length}`
-    )
+    console.log(moviesWithWedgeAntilles.length)
   } catch (parseError) {
     console.error('Error parsing API response:', parseError.message)
     process.exit(1)
