12988fe08a50285dd3784e9d182afe9b3035925f
sam <sookwalinga@gmail.com>
2023-09-13 13:46:57 +0300

    refactor: task4

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -10,22 +10,24 @@ const url = process.argv[2]
 const filePath = process.argv[3]
 const encoding = 'utf-8'
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error.message}`)
-    process.exit(1)
-  }
+const fileStream = fs.createWriteStream(filePath, { encoding })
 
+const req = request.get(url)
+
+req.on('response', (response) => {
   if (response.statusCode !== 200) {
     console.error(`Request failed with status code: ${response.statusCode}`)
     process.exit(1)
   }
 
-  fs.writeFile(filePath, body, encoding, (writeError) => {
-    if (writeError) {
-      console.error(`Error writing to file: ${writeError.message}`)
-      process.exit(1)
-    }
+  response.pipe(fileStream)
+
+  response.on('end', () => {
     console.log(`File saved at ${filePath}`)
   })
+})
+
+req.on('error', (error) => {
+  console.error(`Error: ${error.message}`)
+  process.exit(1)
 })
